0f9c56d84007db24e5a50e0c90624ca3b595d35c
Safa Abidi <61377020+safa-abidi@users.noreply.github.com>
2021-03-03 21:05:56 +0100

    entity and database ready

---
+++ src/main/java/com/safa/employeemanager/model/Employee.java
@@ -0,0 +1,36 @@
+package com.safa.employeemanager.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
